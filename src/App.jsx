d4bea410309883c9690b178ec8c5be36b71d37c6
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-12-17 15:17:25 +0100

    feat: add state for input

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import {useState} from "react";
 import flowers from "./data/flowers";
 
 function App() {
-  const [flow, setFlow] = useState([])
+  const [formValue, setFormValue] = useState(""); // Stato per l'input
+  const [flowerList, setFlowerList] = useState([]); // Stato per la lista dei fiori
 
   // FUNZIONE EVENTO
   const handleFlowersForm = (event) => {
     event.preventDefault();
+    setFlowerList(flowers);
   }
 
   return (
@@ -19,27 +21,18 @@ function App() {
           <div>
             <label htmlFor="fiori">Fiori</label>
             <input type="text" className="form-control" id="fiori"
-             value={flow}
-              onChange={(event) => setFlow(event.target.value)}>
+             value={formValue}
+              onChange={(event) => setFormValue(event.target.value)}>
              </input>
           </div>
+          <button type="submit" className="btn btn-danger mt-3">Conferma</button>
 
         </form>
       </section>
-
-
-
-
-
-
-
-
-
-
-
+      
       <section>
         <div className="row row-cols-2 row-cols-lg-3 mt-5">
-          {flow.map((curItem) => (
+          {flowerList.map((curItem) => (
             <div className="col" key={curItem.id}>
               <div className="card">
                 <h4 className="text-center text-danger pt-2">{curItem.name}</h4>
