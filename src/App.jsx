41e9b368c8a560dea63d02125362afe694dd9c9f
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-12-17 14:59:22 +0100

    feat: add input + map

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,55 @@
+import {useState} from "react";
+import flowers from "./data/flowers";
+
 function App() {
+  const [flow, setFlow] = useState([])
+
+  // FUNZIONE EVENTO
+  const handleFlowersForm = (event) => {
+    event.preventDefault();
+  }
 
   return (
     <>
-     
+
+    <div className="container">
+      <section>
+        <h3 className="text-center text-danger my-3">🌸La lista dei fiori🌸</h3>
+        <form action="" className="" onSubmit={handleFlowersForm}>
+          <div>
+            <label htmlFor="fiori">Fiori</label>
+            <input type="text" className="form-control" id="fiori"
+             value={flow}
+              onChange={(event) => setFlow(event.target.value)}>
+             </input>
+          </div>
+
+        </form>
+      </section>
+
+
+
+
+
+
+
+
+
+
+
+      <section>
+        <div className="row row-cols-2 row-cols-lg-3 mt-5">
+          {flow.map((curItem) => (
+            <div className="col" key={curItem.id}>
+              <div className="card">
+                <h4 className="text-center text-danger pt-2">{curItem.name}</h4>
+                <p className="p-1 text-center">{curItem.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+     </div>
     </>
   )
 }
---
+++ src/data/flowers.js
@@ -0,0 +1,22 @@
+const flowers = [
+  {
+    id: 1,
+    name: "Rosa",
+    description:
+      "Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    id: 2,
+    name: "Margherita",
+    description:
+      "Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    id: 3,
+    name: "Tulipano",
+    description:
+      "Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+];
+
+export default flowers;
