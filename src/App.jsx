580b5e670679e3ce6ef025f653424984d1c6d9d4
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-12-17 16:23:26 +0100

    feat: delete btn

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,18 @@ function App() {
   // FUNZIONE EVENTO
   const handleFlowersForm = (event) => {
     event.preventDefault();
-    setFlowerList(flowers);
+    if(formValue === "fiori") {
+      setFlowerList(flowers);
+    } else {
+      setFlowerList([])
+    }
+    }
+
+
+  // ELIMINARE
+  const flowToDelete = (idDelete) => {
+    const newArray = flowers.filter(curFlow => curFlow.id !== idDelete);
+    setFlowerList(newArray);
   }
 
   return (
@@ -19,30 +30,31 @@ function App() {
         <h3 className="text-center text-danger my-3">🌸La lista dei fiori🌸</h3>
         <form action="" className="" onSubmit={handleFlowersForm}>
           <div>
-            <label htmlFor="fiori">Fiori</label>
-            <input type="text" className="form-control" id="fiori"
+            <label htmlFor="fiori" className="mb-2">Scrivi la parola indicata (Fiori)</label>
+            <input type="text" className="form-control" id="fiori" placeholder="Fiori"
              value={formValue}
               onChange={(event) => setFormValue(event.target.value)}>
              </input>
           </div>
           <button type="submit" className="btn btn-danger mt-3">Conferma</button>
-
         </form>
-      </section>
+        </section>
       
       <section>
-        <div className="row row-cols-2 row-cols-lg-3 mt-5">
+       <div className="row row-cols-2 row-cols-lg-3 mt-3">
           {flowerList.map((curItem) => (
             <div className="col" key={curItem.id}>
-              <div className="card">
+              <div className="card mt-2">
                 <h4 className="text-center text-danger pt-2">{curItem.name}</h4>
                 <p className="p-1 text-center">{curItem.description}</p>
+                <div className="d-flex justify-content-center pb-2">
+                <button onClick={() => flowToDelete(curItem.id)} className="btn btn-outline-danger w-50">Elimina</button></div>
               </div>
             </div>
           ))}
         </div>
-      </section>
-     </div>
+        </section>
+        </div>
     </>
   )
 }
